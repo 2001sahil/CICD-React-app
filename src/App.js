7091b277b920e7235508b25353e9ef79242e58f6
2001sahil <114495568+2001sahil@users.noreply.github.com>
2023-10-01 18:12:28 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          First CI CD PIPELINE CREATED
         </a>
       </header>
     </div>
